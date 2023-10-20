a3e52a24d34ec08fc6af7c53827658045d303d7a
Migue <hector_miguel_moreno_gonzalez@outlook.com>
2023-10-20 14:17:57 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export function App () {
+    return (
+       <article>
+        <header>
+            <img src="https://unavatar.io/zMiguezz/" alt="avatar-migue" />
+            <div>
+                <strong>migue</strong>
+                <span>@zMiguezzz</span>
+            </div>
+        </header>
+
+        <aside>
+            <button>
+                Seguir
+            </button>
+        </aside>
+       </article>
+    )
+}
+
